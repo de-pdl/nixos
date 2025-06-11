dc2198936f88593c490361445ae201b87d781eab
Ayush Paudel <83343839+de-pdl@users.noreply.github.com>
2025-06-11 19:37:11 +1000

    i think finished core now home :)

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,5 +1,12 @@
-{inputs, ...}: {
+{...}: {
   imports = [
     ./boot.nix
+    ./devices.nix
+    ./network.nix
+    ./nix.nix
+    ./services.nix
+    ./time.nix
+    ./utils_base.nix
+    ./users.nix
   ];
 }
