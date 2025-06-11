19856fd317c3ae5f14182e199f08f69086c3bd0a
Ayush Paudel <83343839+de-pdl@users.noreply.github.com>
2025-06-11 19:37:49 +1000

    i think finished core now home.2 forgot to commit :/

---
+++ tmp/modules/core/network.nix
@@ -0,0 +1,38 @@
+{
+  pkgs,
+  host,
+  options,
+  ...
+}: {
+  networking = {
+    hostName = "${host}";
+    networkmanager.enable = true;
+    timeServers = options.networking.timeServers.default ++ ["pool.ntp.org"];
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        22
+        80
+        443
+        59010
+        59011
+        8080
+        1494 # docker
+        2598 # docker
+      ];
+      allowedUDPPorts = [
+        59010
+        59011
+
+        1494 # docker
+        2590 # docker
+      ];
+    };
+  };
+  services.tailscale = {
+    enable = true;
+    useRoutingFeatures = "client";
+  };
+
+  environment.systemPackages = with pkgs; [networkmanagerapplet];
+}
