856b7f2c9fc94b68a43bc87cc67a1bee494e9e99
Ayush Paudel <83343839+de-pdl@users.noreply.github.com>
2025-06-12 15:28:45 +1000

    linked tailscale and ssh to network.nix

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -4,7 +4,6 @@
     libinput.enable = true; # Input Handling
     fstrim.enable = true; # SSD Optimizer
     gvfs.enable = true; # For Mounting USB & More
-    openssh.enable = true; # Enable SSH
     blueman.enable = true; # Bluetooth Support
     tumbler.enable = true; # Image/video preview
     #gnome.gnome-keyring.enable = true;
