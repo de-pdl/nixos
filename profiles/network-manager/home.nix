b80e961a2a75b24213e0876f5cbf8038bd056344
Ayush Paudel <83343839+de-pdl@users.noreply.github.com>
2025-06-25 02:57:35 +1000

    changed

--- profiles/network-manager/home.nix
+++ profiles/network-manager/home.nix
@@ -2,34 +2,36 @@
   profile,
   lib,
   ...
-}: {
-  imports =
-    [
-      ../../modules/home/bash.nix
-      ../../modules/home/bashrc-personal.nix
-      ../../modules/home/fastfetch
-      ../../modules/home/fzf.nix
-      ../../modules/home/git.nix
-      ../../modules/home/gtk.nix
-      ../../modules/home/htop.nix
-      ../../modules/home/kitty.nix
-      ../../modules/home/lazygit.nix
-      ../../modules/home/nvf.nix
-      ../../modules/home/scripts
-      ../../modules/home/yazi
-      ../../modules/home/zsh
-    ]
-    ++ lib.optional profile.guiEnable [
-      ../../modules/home/emoji.nix
-      ../../modules/home/hyprland
-      ../../modules/home/wlogout
-      ../../modules/home/qt.nix
+}: let
+  baseModules = [
+    ../../modules/home/bash.nix
+    ../../modules/home/bashrc-personal.nix
+    ../../modules/home/fastfetch
+    ../../modules/home/fzf.nix
+    ../../modules/home/git.nix
+    ../../modules/home/gtk.nix
+    ../../modules/home/htop.nix
+    ../../modules/home/kitty.nix
+    ../../modules/home/lazygit.nix
+    ../../modules/home/nvf.nix
+    ../../modules/home/scripts
+    ../../modules/home/yazi
+    ../../modules/home/zsh
+  ];
 
-      ../../modules/home/rofi
-      ../../modules/home/swaync.nix
-      ../../modules/home/swappy.nix
-      ../../modules/home/xdg.nix
+  guiModules = lib.optionals profile.guiEnable [
+    ../../modules/home/emoji.nix
+    ../../modules/home/hyprland
+    ../../modules/home/wlogout
+    ../../modules/home/qt.nix
 
-      profile.waybarChoice
-    ];
+    ../../modules/home/rofi
+    ../../modules/home/swaync.nix
+    ../../modules/home/swappy.nix
+    ../../modules/home/xdg.nix
+
+    profile.waybarChoice
+  ];
+in {
+  imports = baseModules ++ guiModules;
 }
