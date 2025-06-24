097143e06afd5455d913b014491970c9e6794cb4
Ayush Paudel <83343839+de-pdl@users.noreply.github.com>
2025-06-25 00:41:14 +1000

    no idea

--- modules/home/hyprland/env.nix
+++ modules/home/hyprland/env.nix
@@ -9,7 +9,7 @@
         "XDG_SESSION_DESKTOP, Hyprland"
         "GDK_BACKEND, wayland, x11"
         "CLUTTER_BACKEND, wayland"
-        "QT_QPA_PLATFORM=wayland;xcb"
+        "QT_QPA_PLATFORM=xcb"
         "QT_WAYLAND_DISABLE_WINDOWDECORATION, 1"
         "QT_AUTO_SCREEN_SCALE_FACTOR, 1"
         "SDL_VIDEODRIVER, x11"
